b6a3b839ddb30e4ad0f20b8d36b4a0fce249135e
abstralexis <alexiswinbox@gmail.com>
2024-11-26 18:01:34 +0000

    Under Construction Page!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+    <title>Alexis' Dev Website</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="The Developer Portfolio/Blog Site for Alexis (abstralexis)">
+    <meta name="author" content="Alexis Williams (abstralexis)"/>
+</svelte:head>
+
+<body class="bg-base text-text mocha">
+    <main>    
+        <div class="space-y-2 text-center">
+            <h1 class="bg-base text-lavender landing-page-title">
+                Welcome!
+            </h1>
+            <h2>
+                My name is Alexis Williams, I'm an 18 year old Developer
+                from the UK. 
+            </h2>
+            <p><i>she/her</i></p>
+        </div>
+
+        <div class="text-center text-text">
+            <pre class="text-rosewater">
+   |\---/|---------------------------------------------------------------
+   | ,_, |                                                               
+    \_`_/-..----.                       Under Construction!              
+ ___/ `   ' ,""+ \           <i>kitty from the ascii art archive</i>     
+(__...'   __\    |`.___.';                                               
+  (_,...'(_,.`__)/'.....+ -----------------------------------------------
+            </pre>
+        </div>
+
+        <div style="position:fixed;width:100%;bottom:0px;left:0px;">
+            <div class="text-center text-text">
+                <small class="text-red">&copy; Alexis Williams 2024</small>
+            </div>
+        </div>
+    </main>
+</body>
