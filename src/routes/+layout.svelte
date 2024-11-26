2a9a5113055237e04d7c491d0de88c6b5d3d6106
abstralexis <alexiswinbox@gmail.com>
2024-11-26 20:09:47 +0000

    Fix HTML + Best Practices

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
     import "../app.css";
 </script>
 
-<slot />
+<main>
+    <slot />
+</main>
+
+<!-- Should I make this be a component? -->
+<footer>
+    <div class="text-center text-text">
+        <small class="text-red">&copy; Alexis Williams 2024</small>
+    </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,35 +4,25 @@
     <meta name="description" content="The Developer Portfolio/Blog Site for Alexis (abstralexis)">
     <meta name="author" content="Alexis Williams (abstralexis)"/>
 </svelte:head>
+ 
+<div class="space-y-2 text-center">
+    <h1 class="bg-base text-lavender landing-page-title">
+        Welcome!
+    </h1>
+    <h2 class="text-text">
+        My name is Alexis Williams, I'm an 18 year old Developer
+        from the UK. 
+    </h2>
+    <p class="text-subtext0"><i>she/her</i></p>
+</div>
 
-<body class="bg-base text-text mocha">
-    <main>    
-        <div class="space-y-2 text-center">
-            <h1 class="bg-base text-lavender landing-page-title">
-                Welcome!
-            </h1>
-            <h2>
-                My name is Alexis Williams, I'm an 18 year old Developer
-                from the UK. 
-            </h2>
-            <p><i>she/her</i></p>
-        </div>
-
-        <div class="text-center text-text">
-            <pre class="text-rosewater">
+<div class="text-center text-text">
+    <pre class="text-rosewater">
    |\---/|---------------------------------------------------------------
    | ,_, |                                                               
     \_`_/-..----.                       Under Construction!              
  ___/ `   ' ,""+ \           <i>kitty from the ascii art archive</i>     
 (__...'   __\    |`.___.';                                               
   (_,...'(_,.`__)/'.....+ -----------------------------------------------
-            </pre>
-        </div>
-
-        <div style="position:fixed;width:100%;bottom:0px;left:0px;">
-            <div class="text-center text-text">
-                <small class="text-red">&copy; Alexis Williams 2024</small>
-            </div>
-        </div>
-    </main>
-</body>
+    </pre>
+</div>
